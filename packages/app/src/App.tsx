858fd09c16bf994d985c6a409d205efb8aa768c0
Ahmet Tuten <74630874+ahmetuten@users.noreply.github.com>
2023-08-03 17:52:02 +0300

    changes

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -27,7 +27,11 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
@@ -38,6 +42,7 @@ import { BackstageOverrides } from '@backstage/core-components';
 import { CssBaseline, ThemeProvider } from '@material-ui/core';
 import { HomepageCompositionRoot } from '@backstage/plugin-home';
 import { HomePageTemplate } from './components/home/HomePage';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 
 export const createCustomThemeOverrides = (
   theme: BackstageTheme,
@@ -114,6 +119,22 @@ const app = createApp({
       ),
     },
   ],
+  components: {
+    SignInPage: props => (
+      <SignInPage
+        {...props}
+        providers={[
+          'guest',
+          {
+            id: 'github-auth-provider',
+            title: 'GitHub',
+            message: 'Sign in using GitHub',
+            apiRef: githubAuthApiRef,
+          },
+        ]}
+      />
+    ),
+  },
 });
 
 const routes = (
