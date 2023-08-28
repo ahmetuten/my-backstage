8dbdbf031fcbd6633ebab581a54a1c6be5e94135
Ahmet Tuten <74630874+ahmetuten@users.noreply.github.com>
2023-08-28 21:59:20 +0300

    changes

--- plugins/kafka-api-backend/src/service/router.ts
+++ plugins/kafka-api-backend/src/service/router.ts
@@ -1,7 +1,8 @@
 import { errorHandler } from '@backstage/backend-common';
-import express from 'express';
+import express, { response } from 'express';
 import Router from 'express-promise-router';
 import { Logger } from 'winston';
+import { Kafka } from 'kafkajs';
 
 export interface RouterOptions {
   logger: Logger;
@@ -15,9 +16,42 @@ export async function createRouter(
   const router = Router();
   router.use(express.json());
 
-  router.get('/health', (_, response) => { // endpoint olusturuluyor
+  router.get('/health', async (req, res) => {
     logger.info('PONG!');
-    response.json({ status: 'ok' }); // dönecek olan response
+    
+    const { topicName } = req.query; // Burada query parametresinden topic adını alıyoruz
+
+    if (!topicName) {
+      return res.status(400).json({ error: 'Missing topicName in query parameters' });
+    }
+
+    try {
+      const kafka = new Kafka({
+          clientId: 'kafka_pub_sub_client',
+          brokers: ["192.168.1.100:9092"]
+      });
+
+      const admin = kafka.admin();
+      logger.info("Broker'a bağlanılıyor...")
+      await admin.connect();
+      logger.info("Broker'a bağlanıldı, topic oluşturulacak...")
+      await admin.createTopics({
+          topics: [
+              {
+                  topic: `${topicName}`,
+                  numPartitions: 1
+              }
+          ]
+      })
+      logger.info("topic oluşturuldu");
+      response.json(({status: "ok"}))
+      await admin.disconnect();
+      } catch (error) {
+        logger.info("bir hata oluştu", error)
+      }finally {
+          process.exit(0);
+      }
+  
   });
   router.use(errorHandler());
   return router;
